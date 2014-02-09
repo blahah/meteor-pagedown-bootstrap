80a8405f581a954bf99df846f53376102098910b
Richard Smith <rds45@cam.ac.uk>
2014-02-09 17:53:09 +0000

    compile less, ensure all buttons same height

--- package.js
+++ package.js
@@ -1,26 +1,27 @@
 
 Package.describe({
-    summary: "Markdown-to-HTML processor",
+    summary: "Markdown-to-HTML processor and editor",
 });
 
 Package.on_use(function (api) {
     api.use('handlebars', 'client');
-
+    var path = Npm.require('path');
+    var asset_path = path.join('lib');
     api.add_files([
       // pagedown
-      'lib/Markdown.Converter.js',
-      'lib/Markdown.Sanitizer.js',
-      'lib/Markdown.Editor.js',
-      'lib/Markdown.Editor.less',
+      path.join(asset_path, 'Markdown.Converter.js'),
+      path.join(asset_path, 'Markdown.Sanitizer.js'),
+      path.join(asset_path, 'Markdown.Editor.js'),
+      path.join(asset_path, 'Markdown.Editor.css'),
 
       // meteor helpers
-      'lib/helpers.js',
+      path.join(asset_path, 'helpers.js'),
+
+      // icons
+	    path.join(asset_path, 'Markdown.Editor.Icons.fw.png'),
+	    path.join(asset_path, 'Markdown.Editor.Icons.png')
 
     ], 'client');
 
-    api.add_files([
-      // icons
-	  'lib/Markdown.Editor.Icons.fw.png',
-	  'lib/Markdown.Editor.Icons.png'
-    ], 'public');
+	api.export( 'Markdown', 'client' );
 });
